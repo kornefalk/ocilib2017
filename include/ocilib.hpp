804e93422dc6123ecf702cd9ad549340839b42ab
Björn Kornefalk <bjorn.kornefalk@gi-de.com>
2017-04-07 08:40:49 +0200

    Add files via upload
    Compiler error using GCC 4.9. ptrdiff_t shall be std::ptrdiff_t

--- include/ocilib.hpp
+++ include/ocilib.hpp
@@ -4938,8 +4938,8 @@ public:
 	typedef Collection<value_type> CollectionType;
 
 	typedef std::random_access_iterator_tag iterator_category;
-	typedef ptrdiff_t difference_type;
-	typedef ptrdiff_t distance_type;
+	typedef std::ptrdiff_t difference_type;
+	typedef std::ptrdiff_t distance_type;
 	typedef value_type* pointer;
 	typedef value_type& reference;
 
